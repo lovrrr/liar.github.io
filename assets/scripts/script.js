419a5cf0f846bf2d6a901fd48e0007ff4026a842
liar <126119963+lovrrr@users.noreply.github.com>
2024-07-01 07:54:48 -0700

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+function videoPlay() {
+  var video = document.getElementById("video");
+  video.volume = 0.05;
+  video.play();
+}
+
+function audioPlay() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.05;
+  audio.play();
+}
